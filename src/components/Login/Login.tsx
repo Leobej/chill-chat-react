f7ffc43b448a31ff282bbcc35d3217aa6a285bb4
Bejgu Leonard Mihai <61775738+Leobej@users.noreply.github.com>
2024-05-05 12:09:27 +0300

    fix(login): add login logic

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import "./Login.css";
+
+interface LoginProps {
+  room: string;
+  setRoom: React.Dispatch<React.SetStateAction<string>>;
+  username: string;
+  setUsername: React.Dispatch<React.SetStateAction<string>>;
+  setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const Login: React.FC<LoginProps> = ({
+  room,
+  setRoom,
+  username,
+  setUsername,
+  setLoggedIn,
+}) => {
+  const checkForLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (room === "" || username === "") {
+      alert("Fill in the required fields");
+    } else {
+      setLoggedIn(true);
+    }
+  };
+
+  return (
+    <div className="login_root">
+      <form className="login_form" onSubmit={checkForLogin}>
+        <input
+          type="text"
+          required
+          placeholder="Room name"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <input
+          type="text"
+          required
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
